a9a3ec9dc90462d15f79dcbf3e7b6705cc849ccd
askprateek <askpk123@gmail.com>
2016-11-18 12:17:33 +0530

    Project Setup

---
+++ src/lib/layout.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class Layout extends React.Component {
+
+  render(){
+    return (
+        <div class="container">
+          <div class="row">
+            <div class = "col s12">
+              <h1>For the Love of Barcharts and Other charts too</h1>
+            </div>
+          </div>
+        </div>
+    );
+  }
+}
